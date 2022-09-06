d1e4a210d0511b2de6e9d6a5f328ad09a8b5d144
katevarvar <110840311+katevarvar@users.noreply.github.com>
2022-09-06 18:42:06 +0300

    Add files via upload

--- Pairs.java
+++ Pairs.java
@@ -2,13 +2,8 @@ package PolishedTasksOneByOne.EqualsHashCodeAndToString.wordMemoriser;
 
 public class Pairs {
     String wordTranslation;
-//    int numbers;
-//    int i = 0;
-
 
     public Pairs(String wordTranslation) {
         this.wordTranslation = wordTranslation;
-//        this.numbers = numbers;
-//        this.i = i;
     }
 }
