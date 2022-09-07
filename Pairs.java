2065a7f45f2b8bb60b44d9c873610be1cda145dc
katevarvar <110840311+katevarvar@users.noreply.github.com>
2022-09-07 16:49:57 +0300

    Delete Pairs.java

--- Pairs.java
+++
@@ -1,9 +0,0 @@
-package PolishedTasksOneByOne.EqualsHashCodeAndToString.wordMemoriser;
-
-public class Pairs {
-    String wordTranslation;
-
-    public Pairs(String wordTranslation) {
-        this.wordTranslation = wordTranslation;
-    }
-}
